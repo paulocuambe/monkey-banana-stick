5fa885fd7f88678f991d7bf9b0e28a1e2087e1ab
Carlos Massavanhane <cmassavanhanejr@gmail.com>
2020-03-31 22:55:02 +0200

    feat (): adapting the code for our reality

--- resolution.pl
+++ resolution.pl
@@ -11,8 +11,43 @@ predicates
 
 
 %Facts
-state(ondoor, onfloor, middle, nosticky ,hasnot).
+do(
+	state(middle, onchair, middle, sticky, hasnot),
+	grasp,
+	state(middle, onchair, middle, nosticky, has)
+).
+
+do(
+	state(P, onfloor, P,sticky, H),
+    climb,
+    state(P, onbox, P, sticky, H)
+ ).
+
+do(
+	state(P, onfloor, P,nosticky, H),
+    takeSticky,
+    state(P, onbox, P, sticky, H)
+).
+do(
+	state(P1, onfloor, P1,nosticky, H),
+    push(P1, P2),
+    state(P2, onfloor, P2,nosticky, H)
+).
+do(
+	state(P1, onfloor, B,nosticky, H),
+    walk(P1, P2),
+    state(P2, onfloor, B, H)
+).
+canget(
+	state(_, _, _, sticky, has)
+).
 
 %Rules
+canget(State1):-
+    do(State1, _, State2),
+    canget(State2).
+
+%queries
+
 
 
