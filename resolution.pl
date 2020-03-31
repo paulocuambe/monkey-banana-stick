138e65adcfb8fc0400fefa633cf6d987e9b0ef96
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-03-31 22:38:44 +0200

    Brief explanation of what is going on inside the file

--- resolution.pl
+++ resolution.pl
@@ -1,3 +1,8 @@
+% Brief explanation of the 'methods' on this file
+% state(MonkeyHorizontalPosition, MonkeyVerticalPosition, BoxPosition, MonkeyHasOrNoBananas)
+% P, P1, P2 - undefined positions the monkey/objects may be
+% H - me dont know if the monkey has or not the banana on this state
+
 move(state(middle, onbox, middle, hasnot),
     grasp,
     state(middle, onbox, middle, has)).
