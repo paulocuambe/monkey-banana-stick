44074769e91f5eac84c38fe881474abfbf9d3097
Carlos Massavanhane <cmassavanhanejr@gmail.com>
2020-03-31 22:35:06 +0200

    style(): adding comments for code

--- resolution.pl
+++ resolution.pl
@@ -1,1 +1,18 @@
-state(ondoor, onchair, middle, nosticky ,hasnot).
+%Monkey_Banana_Sticky problem
+
+/*
+domain
+	name=symbol
+predicates
+	state(name, name, name, name, name)
+	action(name, name, name)
+	canshake(name) 
+*/
+
+
+%Facts
+state(ondoor, onfloor, middle, nosticky ,hasnot).
+
+%Rules
+
+
